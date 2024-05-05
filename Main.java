e9ac979af2f6fa06e7f57538eff578c5f68493e8
lisekxd1234 <lisekxd1234@gmail.com>
2024-05-05 12:53:07 +0200

    Aktualizacja projektu zgodnie z zadaniem 3

--- Main.java
+++ Main.java
@@ -8,12 +8,11 @@ import java.util.List;
 // główna klasa aplikacji
 public class Main {
     public static void main(String[] args) {
-        List<Employee> employees = new ArrayList<>();  // lista przechowująca obiekty pracowników
-        employees.add(new Worker("Jan Kowalski", 3000, 1, "2024-01-01", "Programista"));
-        employees.add(new Worker("Jan Kowalski", 3000, 1, "2024-01-01", "Programista")); // drugi pracownik z tym samym ID
+        List<Employee> employees = new ArrayList<>();  // lista przechowująca obiekty employee
+        employees.add(new Worker("Jan Kowalski", 3000, 1, "2024-01-01", "Developer"));
+        employees.add(new Worker("Anna Nowak", 3500, 2, "2023-12-01", "Designer"));
         employees.add(new Worker("Piotr Zalewski", 3200, 3, "2024-02-01", "Tester"));
-        Manager manager = new Manager("Robert Lewandowski", 5000, 4, "2022-05-01", "Kierownik Zespołu");
-        employees.add(manager);
+        Manager manager = new Manager("Robert Lewandowski", 5000, 4, "2022-05-01", "Team Leader");
 
         // wyświetlanie kodu hash i porównywanie pracowników
         for (Employee employee : employees) {
